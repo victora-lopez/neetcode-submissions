684b2c0ce20be78a31c446d86eb513721ae79ce8
Victor Lopez <victorandreslopez314@gmail.com>
2026-06-25 23:47:31 -0500

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.py
@@ -0,0 +1,22 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack:
+            self.minStack.append(val)
+        elif val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.minStack[-1]:
+            self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
